de34b9feaa99301cea3cef4e13a2c2bbe6749e94
Kevin Ahrendt <kahrendt@gmail.com>
2024-08-11 11:56:27 -0400

    Use PSRAM for internal buffers if available (#1)
    * add support for external psram
    
    * don't cast malloc pointer
    
    * fall back to internal memory
    
    * bump version

--- src/filterbank_util.c
+++ src/filterbank_util.c
@@ -1,4 +1,5 @@
 /* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+Modifications copyright 2024 Kevin Ahrendt.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,10 +19,15 @@ limitations under the License.
 #include <math.h>
 #include <stdio.h>
 
+#ifdef USE_ESP32
+#include <esp_heap_caps.h>
+#endif
+
 #define kFilterbankIndexAlignment 4
 #define kFilterbankChannelBlockSize 4
 
-void FilterbankFillConfigWithDefaults(struct FilterbankConfig *config) {
+void FilterbankFillConfigWithDefaults(struct FilterbankConfig *config)
+{
   config->num_channels = 32;
   config->lower_band_limit = 125.0f;
   config->upper_band_limit = 7500.0f;
@@ -31,27 +37,31 @@ void FilterbankFillConfigWithDefaults(struct FilterbankConfig *config) {
 static float FreqToMel(float freq) { return 1127.0f * log1pf(freq / 700.0f); }
 
 static void CalculateCenterFrequencies(const int num_channels, const float lower_frequency_limit,
-                                       const float upper_frequency_limit, float *center_frequencies) {
+                                       const float upper_frequency_limit, float *center_frequencies)
+{
   assert(lower_frequency_limit >= 0.0f);
   assert(upper_frequency_limit > lower_frequency_limit);
 
   const float mel_low = FreqToMel(lower_frequency_limit);
   const float mel_hi = FreqToMel(upper_frequency_limit);
   const float mel_span = mel_hi - mel_low;
-  const float mel_spacing = mel_span / ((float) num_channels);
+  const float mel_spacing = mel_span / ((float)num_channels);
   int i;
-  for (i = 0; i < num_channels; ++i) {
+  for (i = 0; i < num_channels; ++i)
+  {
     center_frequencies[i] = mel_low + (mel_spacing * (i + 1));
   }
 }
 
-static void QuantizeFilterbankWeights(const float float_weight, int16_t *weight, int16_t *unweight) {
+static void QuantizeFilterbankWeights(const float float_weight, int16_t *weight, int16_t *unweight)
+{
   *weight = floorf(float_weight * (1 << kFilterbankBits) + 0.5f);
   *unweight = floorf((1.0f - float_weight) * (1 << kFilterbankBits) + 0.5f);
 }
 
 int FilterbankPopulateState(const struct FilterbankConfig *config, struct FilterbankState *state, int sample_rate,
-                            int spectrum_size) {
+                            int spectrum_size)
+{
   state->num_channels = config->num_channels;
   const int num_channels_plus_1 = config->num_channels + 1;
 
@@ -59,18 +69,69 @@ int FilterbankPopulateState(const struct FilterbankConfig *config, struct Filter
   const int index_alignment =
       (kFilterbankIndexAlignment < sizeof(int16_t) ? 1 : kFilterbankIndexAlignment / sizeof(int16_t));
 
-  state->channel_frequency_starts = (int16_t *) malloc(num_channels_plus_1 * sizeof(*state->channel_frequency_starts));
-  state->channel_weight_starts = (int16_t *) malloc(num_channels_plus_1 * sizeof(*state->channel_weight_starts));
-  state->channel_widths = (int16_t *) malloc(num_channels_plus_1 * sizeof(*state->channel_widths));
-  state->work = (uint64_t *) malloc(num_channels_plus_1 * sizeof(*state->work));
+#ifdef USE_ESP32
+  state->channel_frequency_starts = heap_caps_malloc(num_channels_plus_1 * sizeof(*state->channel_frequency_starts), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  if (state->channel_frequency_starts == NULL)
+#endif
+  {
+    state->channel_frequency_starts = malloc(num_channels_plus_1 * sizeof(*state->channel_frequency_starts));
+  }
 
-  float *center_mel_freqs = (float *) malloc(num_channels_plus_1 * sizeof(*center_mel_freqs));
-  int16_t *actual_channel_starts = (int16_t *) malloc(num_channels_plus_1 * sizeof(*actual_channel_starts));
-  int16_t *actual_channel_widths = (int16_t *) malloc(num_channels_plus_1 * sizeof(*actual_channel_widths));
+#ifdef USE_ESP32
+  state->channel_weight_starts = heap_caps_malloc(num_channels_plus_1 * sizeof(*state->channel_weight_starts), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  if (state->channel_weight_starts == NULL)
+#endif
+  {
+    state->channel_weight_starts = malloc(num_channels_plus_1 * sizeof(*state->channel_weight_starts));
+  }
+
+#ifdef USE_ESP32
+  state->channel_widths = heap_caps_malloc(num_channels_plus_1 * sizeof(*state->channel_widths), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  if (state->channel_widths == NULL)
+#endif
+  {
+    state->channel_widths = malloc(num_channels_plus_1 * sizeof(*state->channel_widths));
+  }
+
+#ifdef USE_ESP32
+  state->work = heap_caps_malloc(num_channels_plus_1 * sizeof(*state->work), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  if (state->work == NULL)
+#endif
+  {
+    state->work = malloc(num_channels_plus_1 * sizeof(*state->work));
+  }
+
+  float *center_mel_freqs = NULL;
+#ifdef USE_ESP32
+  center_mel_freqs = heap_caps_malloc(num_channels_plus_1 * sizeof(*center_mel_freqs), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  if (center_mel_freqs == NULL)
+#endif
+  {
+    center_mel_freqs = malloc(num_channels_plus_1 * sizeof(*center_mel_freqs));
+  }
+
+  int16_t *actual_channel_starts = NULL;
+#ifdef USE_ESP32
+  actual_channel_starts = heap_caps_malloc(num_channels_plus_1 * sizeof(*actual_channel_starts), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  if (actual_channel_starts == NULL)
+#endif
+  {
+    actual_channel_starts = malloc(num_channels_plus_1 * sizeof(*actual_channel_starts));
+  }
+
+  int16_t *actual_channel_widths = NULL;
+#ifdef USE_ESP32
+  actual_channel_widths = heap_caps_malloc(num_channels_plus_1 * sizeof(*actual_channel_widths), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+  if (actual_channel_widths == NULL)
+#endif
+  {
+    actual_channel_widths = malloc(num_channels_plus_1 * sizeof(*actual_channel_widths));
+  }
 
   if (state->channel_frequency_starts == NULL || state->channel_weight_starts == NULL ||
       state->channel_widths == NULL || center_mel_freqs == NULL || actual_channel_starts == NULL ||
-      actual_channel_widths == NULL) {
+      actual_channel_widths == NULL)
+  {
     free(center_mel_freqs);
     free(actual_channel_starts);
     free(actual_channel_widths);
@@ -81,9 +142,9 @@ int FilterbankPopulateState(const struct FilterbankConfig *config, struct Filter
   CalculateCenterFrequencies(num_channels_plus_1, config->lower_band_limit, config->upper_band_limit, center_mel_freqs);
 
   // Always exclude DC.
-  const float hz_per_sbin = 0.5f * sample_rate / ((float) spectrum_size - 1);
+  const float hz_per_sbin = 0.5f * sample_rate / ((float)spectrum_size - 1);
   state->start_index = 1.5f + config->lower_band_limit / hz_per_sbin;
-  state->end_index = 0;  // Initialized to zero here, but actually set below.
+  state->end_index = 0; // Initialized to zero here, but actually set below.
 
   // For each channel, we need to figure out what frequencies belong to it, and
   // how much padding we need to add so that we can efficiently multiply the
@@ -96,10 +157,12 @@ int FilterbankPopulateState(const struct FilterbankConfig *config, struct Filter
   int needs_zeros = 0;
 
   int chan;
-  for (chan = 0; chan < num_channels_plus_1; ++chan) {
+  for (chan = 0; chan < num_channels_plus_1; ++chan)
+  {
     // Keep jumping frequencies until we overshoot the bound on this channel.
     int freq_index = chan_freq_index_start;
-    while (FreqToMel((freq_index) *hz_per_sbin) <= center_mel_freqs[chan]) {
+    while (FreqToMel((freq_index)*hz_per_sbin) <= center_mel_freqs[chan])
+    {
       ++freq_index;
     }
 
@@ -107,7 +170,8 @@ int FilterbankPopulateState(const struct FilterbankConfig *config, struct Filter
     actual_channel_starts[chan] = chan_freq_index_start;
     actual_channel_widths[chan] = width;
 
-    if (width == 0) {
+    if (width == 0)
+    {
       // This channel doesn't actually get anything from the frequencies, it's
       // always zero. We need then to insert some 'zero' weights into the
       // output, and just redirect this channel to do a single multiplication at
@@ -117,15 +181,19 @@ int FilterbankPopulateState(const struct FilterbankConfig *config, struct Filter
       state->channel_frequency_starts[chan] = 0;
       state->channel_weight_starts[chan] = 0;
       state->channel_widths[chan] = kFilterbankChannelBlockSize;
-      if (!needs_zeros) {
+      if (!needs_zeros)
+      {
         needs_zeros = 1;
         int j;
-        for (j = 0; j < chan; ++j) {
+        for (j = 0; j < chan; ++j)
+        {
           state->channel_weight_starts[j] += kFilterbankChannelBlockSize;
         }
         weight_index_start += kFilterbankChannelBlockSize;
       }
-    } else {
+    }
+    else
+    {
       // How far back do we need to go to ensure that we have the proper
       // alignment?
       const int aligned_start = (chan_freq_index_start / index_alignment) * index_alignment;
@@ -143,11 +211,12 @@ int FilterbankPopulateState(const struct FilterbankConfig *config, struct Filter
   // Allocate the two arrays to store the weights - weight_index_start contains
   // the index of what would be the next set of weights that we would need to
   // add, so that's how many weights we need to allocate.
-  state->weights = (int16_t *) calloc(weight_index_start, sizeof(*state->weights));
-  state->unweights = (int16_t *) calloc(weight_index_start, sizeof(*state->unweights));
+  state->weights = (int16_t *)calloc(weight_index_start, sizeof(*state->weights));
+  state->unweights = (int16_t *)calloc(weight_index_start, sizeof(*state->unweights));
 
   // If the alloc failed, we also need to nuke the arrays.
-  if (state->weights == NULL || state->unweights == NULL) {
+  if (state->weights == NULL || state->unweights == NULL)
+  {
     free(center_mel_freqs);
     free(actual_channel_starts);
     free(actual_channel_widths);
@@ -159,7 +228,8 @@ int FilterbankPopulateState(const struct FilterbankConfig *config, struct Filter
   // zero, we only need to fill in the weights that correspond to some frequency
   // for a channel.
   const float mel_low = FreqToMel(config->lower_band_limit);
-  for (chan = 0; chan < num_channels_plus_1; ++chan) {
+  for (chan = 0; chan < num_channels_plus_1; ++chan)
+  {
     int frequency = actual_channel_starts[chan];
     const int num_frequencies = actual_channel_widths[chan];
     const int frequency_offset = frequency - state->channel_frequency_starts[chan];
@@ -167,7 +237,8 @@ int FilterbankPopulateState(const struct FilterbankConfig *config, struct Filter
     const float denom_val = (chan == 0) ? mel_low : center_mel_freqs[chan - 1];
 
     int j;
-    for (j = 0; j < num_frequencies; ++j, ++frequency) {
+    for (j = 0; j < num_frequencies; ++j, ++frequency)
+    {
       const float weight =
           (center_mel_freqs[chan] - FreqToMel(frequency * hz_per_sbin)) / (center_mel_freqs[chan] - denom_val);
 
@@ -175,7 +246,8 @@ int FilterbankPopulateState(const struct FilterbankConfig *config, struct Filter
       const int weight_index = weight_start + frequency_offset + j;
       QuantizeFilterbankWeights(weight, state->weights + weight_index, state->unweights + weight_index);
     }
-    if (frequency > state->end_index) {
+    if (frequency > state->end_index)
+    {
       state->end_index = frequency;
     }
   }
@@ -183,14 +255,16 @@ int FilterbankPopulateState(const struct FilterbankConfig *config, struct Filter
   free(center_mel_freqs);
   free(actual_channel_starts);
   free(actual_channel_widths);
-  if (state->end_index >= spectrum_size) {
+  if (state->end_index >= spectrum_size)
+  {
     fprintf(stderr, "Filterbank end_index is above spectrum size.\n");
     return 0;
   }
   return 1;
 }
 
-void FilterbankFreeStateContents(struct FilterbankState *state) {
+void FilterbankFreeStateContents(struct FilterbankState *state)
+{
   free(state->channel_frequency_starts);
   free(state->channel_weight_starts);
   free(state->channel_widths);
